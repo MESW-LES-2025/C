6695f64593bf488866ea477142283eb2e47cd8f3
José Costa <146367084+JoseCostaUni@users.noreply.github.com>
2025-12-05 22:45:09 +0100

    Task/frontend/list cases lawyer (#236)
    * Implement cases overview layout
    
    * Added search bar and start process button
    
    * Fix between start process button and the search bar
    
    * Added case details
    
    * Fixed client details layout (not done yet)
    
    * Polishing
    
    * refactor: update all Cases-related components, pages and routes to Processes
    
    * refactor: new pagination bar
    
    * implemented compact pagination ("1 2 3 4 ... last")
    
    * refactor: improved dynamic pagination with current-page window
    
    * refactor: update pagination styles
    
    * feat: add ProcessDetails component and routing, enhance Processes page with navigation
    
    * feat: enhance ProcessDetails component with client information display
    
    * feat: add documents and doc upload
    
    * feat: add document type icons for .docx and .pdf files
    
    * feat: add send button and chat input field
    
    * changed names between cases and processes
    
    * implemented role-based routing for processes (lawyer view enabled)
    
    * add create process functionality for lawyers  and all listing processes for each of them
    
    * add processes client view
    
    * update lawyer display and client name loading in processes views
    
    * add process details view and role-based display for client and lawyer (not finished yet)
    
    * implemented file upload functionality
    
    * refactor processes handling: aggregate lawyer and client views
    
    * breadcrumb service wokring on process details
    
    * enhance process details view: add some empty states messages
    
    * implement search functionality for processes
    
    * add process status and type/phase details to process details view
    
    * refactor process main layout: adjust overflow styles
    
    * update navigation paths in create and processes components
    
    * add status badge to process details view with dynamic styling
    
    * add client assignment feature to process creation form
    
    * add document deletion endpoint and update process service to handle document removal
    
    ---------
    
    Co-authored-by: Letícia Coelho <leticiamendes2122@gmail.com>

--- src/frontend/src/app/app.routes.ts
+++ src/frontend/src/app/app.routes.ts
@@ -5,7 +5,6 @@ import { ClientDetailsComponent } from './pages/client-details/client-details';
 import { LawyerDetailsComponent } from './pages/lawyer-details/lawyer-details';
 import { LawyersComponent } from './pages/lawyers';
 import { ChatbotComponent } from './pages/chatbot';
-import { CasesComponent } from './pages/cases';
 import { ProfilesComponent } from './pages/profiles';
 import { SettingsComponent } from './pages/settings';
 import { CreateClientComponent } from './pages/create-client/create-client';
@@ -15,6 +14,9 @@ import { NgModule } from '@angular/core';
 import { EditClientComponent } from './pages/edit-client/edit-client';
 import { EditLawyerComponent } from './pages/edit-lawyer/edit-lawyer';
 import { roleGuard } from './guards/role.guard';
+import { CreateProcessComponent } from './pages/create-process/create-process';
+import { ProcessDetailsComponent } from './pages/process-details/process-details';
+import { ProcessesComponent } from './pages/processes/processes';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent, title: 'Login' },
@@ -63,11 +65,25 @@ export const routes: Routes = [
   },
   { path: 'chatbot', component: ChatbotComponent, title: 'ChatBot', canActivate: [roleGuard] },
   {
-    path: 'cases',
-    component: CasesComponent,
-    title: 'Cases',
+    path: 'processes',
+    component: ProcessesComponent,
+    title: 'Processes',
     canActivate: [roleGuard],
-    data: { roles: ['Admin', 'Lawyer', 'Client'] },
+    data: { roles: ['Client', 'Lawyer', 'Admin'] },
+  },
+  {
+    path: 'processes/create',
+    component: CreateProcessComponent,
+    title: 'Create Process',
+    canActivate: [roleGuard],
+    data: { roles: ['Lawyer', 'Admin'] }
+  },
+  {
+    path: 'processes/:id',
+    component: ProcessDetailsComponent,
+    title: 'Process Details',
+    canActivate: [roleGuard],
+    data: { roles: ['Client', 'Lawyer', 'Admin'] }
   },
   { path: 'profiles', component: ProfilesComponent, title: 'Profiles', canActivate: [roleGuard] },
   { path: 'settings', component: SettingsComponent, title: 'Settings', canActivate: [roleGuard] },
